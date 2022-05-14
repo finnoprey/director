7927b9f20489da66048836bb41fed7e1b372d6d9
Finn O'Prey <finn@oprey.co>
2022-05-14 13:31:17 +1000

    Added login and basic redirects via middleware

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,14 +1,33 @@
 import { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
 
-export function middleware(req, ev) {
-  const PERMITTED = ['/dashboard', '/']
+export async function middleware(req, ev) {
+  const PERMITTED = ['/dashboard', '/', '/login']
 
   let path = req.nextUrl.pathname
 
   if (PERMITTED.includes(path)) {
     return
   }
+  
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY
 
-  console.log()
-  return NextResponse.redirect('https://finn.so')
+  const response = await fetch(supabaseUrl + '/rest/v1/redirects', {
+    headers: {
+      'Content-Type': 'application/json',
+      'apikey': supabaseKey,
+      'Authorization': 'Bearer ' + supabaseKey,
+    }
+  })
+
+  if (response) {
+    const data = await response.json()
+
+    if (data) {
+      const redirect = data.find((r) => {
+        return r.from == path.replace('/', '')
+      })
+      return NextResponse.redirect(redirect.to)
+    }
+  }
 }
