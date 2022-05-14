0306fe72d6234707a1653449b3f33820a4bacb6b
Finn O'Prey <finn@oprey.co>
2022-05-14 16:03:43 +1000

    Removed account components & made shorten page ui

---
+++ pages/shorten.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import { useState, useEffect } from 'react'
+
+import styles from '../styles/Shorten.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Director - Shortener</title>
+        <meta name="description" content="Redirect management application." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Shortener</h1>
+        <form className={styles.form}>
+          <input className={styles.url} type="text" name="url" />
+          <button className={styles.button}>
+            Go
+          </button>
+        </form>
+      </div>
+    </div>
+  )
+}
